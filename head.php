db95389074445583c38b7259562324a2a2bdfddb
Brent Krone-Schmidt <bawa@deviat.net>
2015-05-10 20:52:08 -0700

    add mobile

--- head.php
+++ head.php
@@ -5,7 +5,8 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8"> 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
 	<title>TKT</title>
 	<style type="text/css">
 		@import url('inc/theme.css');
